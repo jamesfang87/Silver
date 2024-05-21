e795d107edd98b8cb2e3a893501523dccf616ee9
James Fang <113867753+jamesfang87@users.noreply.github.com>
2024-05-20 23:56:50 -0500

    Update main.cpp

--- Two Pointers/Garland/main.cpp
+++ Two Pointers/Garland/main.cpp
@@ -8,7 +8,7 @@ void solve(string garland) {
     int start = 0, end = 0;
     int used_edits = 0;
     int ans = INT_MIN;
-    while (start < garland.length() && end < garland.length()) {
+    for (; start < garland.length(); start++) {
         // use up all edits
         while (end < garland.length() && used_edits < total_edits) {
             if (garland[end] != fav_color) {
@@ -16,7 +16,7 @@ void solve(string garland) {
             }
             end++;
         }
-
+        
         // sometimes we have used all edits but have connected two segments of fav colors
         // take this into account
         while (end < garland.length() && garland[end] == fav_color) {
@@ -24,19 +24,18 @@ void solve(string garland) {
         }
 
         ans = max(ans, end - start);
-
-        if (start + 1 <= end && start < garland.length()) {
-            if (garland[start] != fav_color) {
-                used_edits--;
-            }
-            start++;
+        
+        if (garland[start] != fav_color) {
+            used_edits--;
         }
     }
-
     cout << ans << '\n';
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int len; string garland;
     cin >> len >> garland;
 
