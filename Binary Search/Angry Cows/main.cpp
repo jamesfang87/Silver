55f89439b8e3ed07d97f8afa283be8b1861be05f
James Fang <113867753+jamesfang87@users.noreply.github.com>
2024-05-28 23:59:47 -0500

    Update main.cpp

--- Binary Search/Angry Cows/main.cpp
+++ Binary Search/Angry Cows/main.cpp
@@ -17,6 +17,22 @@ bool possible(long long radius, const vector<long long>& positions, long long k)
 }
 
 int main() {
+    /*
+     * binary search on the radius
+     *
+     * hard part is figuring if a combination of R and K work
+     * to maximize the amount of haybales that are exploded,
+     * we start at the lowest position and have that be the endpoint of the explosion
+     * essentially we aim a cow at pos x = position + R
+     *
+     * we continue this until we run out of cows, or we blow up the last haybale
+     *
+     * it's possible to do this in O(n) time with a while loop
+     * we can also do this in O(logn) time using std::upper_bound
+     * we just have to check with each iteration rather than just lazily at the end
+     * since positions.end(), which is returned by std::upper_bound when no elements are greater
+     * could be negative meaning std::upper_bound returns positions.begin() the next iteration
+     */
     ifstream fin("angry.in");
     ofstream fout("angry.out");
 
@@ -27,7 +43,6 @@ int main() {
         fin >> pos;
     }
     sort(positions.begin(), positions.end());
-
 
     long long low = 1, high = positions[n - 1] + 1;
     while (low < high) {
