f756e0caef40769c6336326537e13c7d8fb3e53a
James Fang <113867753+jamesfang87@users.noreply.github.com>
2024-06-10 13:39:22 -0500

    Update main.cpp

--- Prefix Sums/GCD on Blackboard/main.cpp
+++ Prefix Sums/GCD on Blackboard/main.cpp
@@ -14,17 +14,17 @@ int main() {
         reversed_numbers[i] = numbers[n - 1 - i];
     }
 
-    int gcd[n + 1]; gcd[0] = 0;
-    int reverse_gcd[n + 1]; reverse_gcd[0] = 0;
+    int forward_gcd[n + 1]; gcd[0] = 0;
+    int reversed_gcd[n + 1]; reversed_gcd[0] = 0;
     for (int i = 0; i < n; i++) {
-        gcd[i + 1] = __gcd(gcd[i], numbers[i]);
-        reverse_gcd[i + 1] = __gcd(reverse_gcd[i], reversed_numbers[i]);
+        forward_gcd[i + 1] = __gcd(forward_gcd[i], numbers[i]);
+        reversed_gcd[i + 1] = __gcd(reversed_gcd[i], reversed_numbers[i]);
     }
 
     int ans = 0;
     for (int i = 0; i < n; i++) {
         int reversed = n - i;
-        ans = max(ans, __gcd(reverse_gcd[reversed - 1], gcd[i]));
+        ans = max(ans, __gcd(reverse_gcd[reversed - 1], forwawrd_gcd[i]));
     }
     cout << ans;
 }
