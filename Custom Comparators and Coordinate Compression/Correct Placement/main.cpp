096032ef7ee21398bc9dc924abedc35da297229d
jamesfang87 <james.fang.713@gmail.com>
2024-08-13 22:01:16 -0500

    awahsdfad

--- Custom Comparators and Coordinate Compression/Correct Placement/main.cpp
+++ Custom Comparators and Coordinate Compression/Correct Placement/main.cpp
@@ -1,17 +1,21 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <tuple>
+#include <climits>
 using namespace std;
 
 #define all(x) x.begin(), x.end()
 
 struct Friend {
-    int h, w, id;
+    int h, w, index;
 };
 
 bool operator<(const Friend& lhs, const Friend& rhs) {
-    return std::tie(lhs.h, lhs.w, lhs.id) < std::tie(rhs.h, rhs.w, rhs.id);
+    return std::tie(lhs.h, lhs.w, lhs.index) < std::tie(rhs.h, rhs.w, rhs.index);
 }
 
-int search(Friend& f, const vector<Friend>& friends, const vector<pair<Friend, Friend>>& prefix_mins) {
+int search(Friend& f, vector<Friend>& friends, vector<pair<Friend, Friend>>& prefix_mins) {
     int l = -1, r = friends.size() - 1;
     while (l < r) {
         int m = l + (r - l + 1) / 2;
@@ -21,16 +25,16 @@ int search(Friend& f, const vector<Friend>& friends, const vector<pair<Friend, F
             r = m - 1;
         }
     }
-    
+
     if (l == -1) {
         return -1;
     }
- 
-    auto [smallest, sec_smallest] = prefix_mins[l + 1];
-    if (smallest.id != f.id) {
-        return smallest.w < f.w ? smallest.id : -1;
+
+    auto [first_choice, second_choice] = prefix_mins[l + 1];
+    if (first_choice.index != f.index) {
+        return first_choice.w < f.w ? first_choice.index : -1;
     } else {
-        return sec_smallest.w < f.w ? sec_smallest.id : -1;
+        return second_choice.w < f.w ? second_choice.index : -1;
     }
 }
 
@@ -39,28 +43,22 @@ void solve() {
     vector<Friend> unsorted_friends(n);
     for (int i = 0; i < n; i++) {
         cin >> unsorted_friends[i].h >> unsorted_friends[i].w;
-        unsorted_friends[i].id = i + 1;
+        unsorted_friends[i].index = i + 1;
     }
 
     vector<Friend> friends = unsorted_friends;
     sort(friends.begin(), friends.end());
 
-    vector<pair<Friend, Friend>> prefix_mins = {
-        {   
-            {INT_MAX, INT_MAX, -1}, 
-            {INT_MAX, INT_MAX, -1}
-        }
-    };
-    for (int i = 0; i < n; i++) {
-        auto [smallest, sec_smallest] = prefix_mins[i];
-        if (friends[i].w < smallest.w) {
-            sec_smallest = smallest;
-            smallest = friends[i];
-        } else {
-            sec_smallest = min(sec_smallest, friends[i]);
-        }
-        prefix_mins.push_back({smallest, sec_smallest});
+    vector<pair<Friend, Friend>> prefix_mins = {{{INT_MAX, INT_MAX, -1}, {INT_MAX, INT_MAX, -1}}};
+    for (int i = 0; i < friends.size(); i++) {
+        Friend least_w = prefix_mins[i].first, second_least_w = prefix_mins[i].second;
+
+        least_w = friends[i].w <= least_w.w ? friends[i] : least_w;
+        second_least_w = friends[i].w < second_least_w.w ? friends[i] : second_least_w;
+
+        prefix_mins.push_back({least_w, second_least_w});
     }
+
 
     for (auto f : unsorted_friends) {
         int result = search(f, friends, prefix_mins);
@@ -75,6 +73,7 @@ void solve() {
             cout << result << ' ';
             goto cont;
         }
+        swap(f.h, f.w);
         
         cout << "-1 ";
         cont:;
