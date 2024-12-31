50ac738ff909a98c0eeafab72ad7724d1343b867
jamesfang87 <james.fang.713@gmail.com>
2024-12-31 00:41:07 -0600

    levle gen

--- Binary Search/Level Generation/main.cpp
+++ Binary Search/Level Generation/main.cpp
@@ -2,20 +2,37 @@
 #include <iostream>
 using namespace std;
 
-bool possible(int edges, int nodes) {
-    int bridges = (edges + 1) / 2;
-    int edges_left = edges - bridges;
-    
-    int free_nodes = nodes;
+bool possible(long long total_edges, long long nodes) {
+    long long bridges_needed = (total_edges + 1) / 2;
+    long long edges_left = total_edges - bridges_needed;
+
+    if (nodes < bridges_needed) {
+        return false;
+    }
+
+    long long free_nodes = nodes - bridges_needed;
     return edges_left <= (free_nodes - 1) * free_nodes / 2;
 }
 
-int solve() {
-    int n; cin >> n;
-    
-    int l = 0, r = INT32_MAX - 1;
+long long solve() {
+    // an easy (and sneaky) way to cheat the problem is
+    // create a tree with depth 1 rooted at some arbitrary node
+    // then, the number of nodes in the tree is 1 + the number of bridges needed
+    // basically, if you need 2 bridges and you choose node 1 as your root,
+    // then your tree looks 1 - 2 and 1 - 3. then, the rest of the nodes
+    // are connected to the root.
+    int n;
+    cin >> n;
+
+    // be sure to properly handle the edge case of n = 1,
+    // since this way doesn't really work with just 1 node
+    if (n == 1) {
+        return 0;
+    }
+
+    long long l = -1, r = INT64_MAX;
     while (l < r) {
-        int m = l + (r - l + 1) / 2;
+        long long m = l + (r - l + 1) / 2;
         if (possible(m, n)) {
             l = m;
         } else {
@@ -28,8 +45,9 @@ int solve() {
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    int q; cin >> q;
+
+    int q;
+    cin >> q;
     while (q--) {
         cout << solve() << '\n';
     }
